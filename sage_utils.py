52c1ec320947b9bcd592a29e9419b8bd715af866
Ryan Krauss <ryanlists@gmail.com>
2011-08-24 14:30:40 -0500

    sage stuff

---
+++ sage_utils.py
@@ -0,0 +1,63 @@
+import sympy, copy
+
+def Symbol_filt(linein):
+    if linein.find('Symbol(') > -1:
+        return False
+    else:
+        return True
+
+
+def _clean_Symbol(listin, make_copy=False):
+    if make_copy:
+        out_list = copy.copy(listin)
+        out_list = filter(Symbol_filt, out_list)
+    else:
+        out_list = filter(Symbol_filt, listin)
+    return out_list
+
+
+def sage_poly_to_python_list(sage_poly, s=None):
+    """Convert a symbolic sage expression in s to a python list of
+    coeffs."""
+    if s is None:
+        s = sympy.Symbol('s')
+    sympy_poly = sympy.Poly(sage_poly,s)
+    
+    coeff_str = sympy.python(sympy_poly.coeffs)
+    coeff_list = coeff_str.split('\n')
+    coeff_list = _clean_Symbol(coeff_list)
+    return coeff_list
+
+
+def sympy_code_list_to_useable_code(codelist, outname):
+    assert len(codelist) == 1, "got codelist whose length != 1"
+    codeline = codelist[0]
+    assert codeline.find('e = ') == 0, "codeline doesn't start with 'e = '"
+    outline = outname + ' = ' + codeline[4:]
+    return outline
+
+    
+def sage_tf_to_python(tf, name_out='tf', s=None, substr=''):
+    N = tf.numerator()
+    D = tf.denominator()
+    Nsympy = sympy.sympify(N)
+    Dsympy = sympy.sympify(D)
+    num_list = sage_poly_to_python_list(Nsympy)
+    den_list = sage_poly_to_python_list(Dsympy)
+
+    num_name = 'num'
+    den_name = 'den'
+    if substr:
+        num_name += '_' + substr
+        den_name += '_' + substr
+        name_out += '_' + substr
+
+    out_list = []
+    num_line = sympy_code_list_to_useable_code(num_list, num_name)
+    out_list.append(num_line)
+    den_line = sympy_code_list_to_useable_code(den_list, den_name)
+    out_list.append(den_line)
+    last_line = '%s = controls.TF(%s, %s)' % (name_out, num_name, den_name)
+    out_list.append(last_line)
+    
+    return out_list
