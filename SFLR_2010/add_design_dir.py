460e0f47bef9a4c4c998ad08cdea126e2dd81cda
Ryan Krauss <ryanlists@gmail.com>
2015-04-14 15:30:15 -0500

    trying to get JCF_Design for DSCC 2015 working on the RPi

--- SFLR_2010/add_design_dir.py
+++ SFLR_2010/add_design_dir.py
@@ -1,6 +1,6 @@
 import sys, rwkos
 
-design_dir = rwkos.FindFullPath('SFLR_2010/vibration_suppression_design/TMM')
+design_dir = rwkos.FindFullPath('siue/Research/SFLR_2010/vibration_suppression_design/TMM')
 
 if design_dir not in sys.path:
     sys.path.append(design_dir)
